19f4954d45b5223ad608c29c3a97a2fea83a50c7
Robert Burnett <dr.slate2@gmail.com>
2025-02-15 13:02:25 -0600

    a few experiments in make.h and defer.h

--- src/defer.h
+++ src/defer.h
@@ -11,3 +11,98 @@
 #define deferred(label) do { goto label; label##_done_:; } while (0)
 
 #endif
+
+
+
+
+#if 0
+int toggle() {
+    static int state = 0; // Static variable retains value between function calls
+    return state++ < 2;   // Increments and returns true for the first two calls
+}
+
+while(toggle())
+    if(i == 1) {
+        /*free memory*/
+    } else {
+        /*use memory*/
+    }
+
+#define later \
+    while(toggle())
+
+#define first else
+
+int main() {
+    char * mem = malloc(100);
+
+    later {
+        free(mem);
+    } first {
+        int i = 0; 
+        for(i = 0; i < 100; ++i) {
+            
+        }
+    }
+
+#endif
+
+#if 0
+typedef enum {
+    LATCH_GET,
+    LATCH_TOGGLE,
+    LATCH_ON,
+    LATCH_OFF,
+} LatchCommand;
+
+int latch(LatchCommand cmd) {
+    static int state = 0;
+    if(cmd == LATCH_GET) {
+        return state;
+    } else if(cmd == LATCH_TOGGLE) {
+        state = !state;
+    } else if(cmd == LATCH_ON) {
+        state = 1;
+    } else if(cmd == LATCH_OFF) {
+        state = 0; 
+    } else {
+        abort();
+    }
+}
+
+#define after(statement) \
+    for(latch(LATCH_ON); latch(LATCH_GET); latch(LATCH_OFF))
+
+#define finally(statement) \
+    for(;;(statement, 0)) mylabel: 
+}
+
+int main() {
+    char * mem = malloc(100);
+    finally(free(mem)) {
+        int i = 0; 
+        for(i = 0; i < 100; ++i) {
+            printf("%c", mem[i]);    
+        }
+    }
+
+}
+
+#define eventually
+
+#define butfirst
+
+int main() {
+
+    char * mem = malloc(100);
+    eventually {
+        free(mem);
+    } butfirst {
+        int i = 0; 
+        for(i = 0; i < 100; ++i) {
+            printf("%c", mem[i]);    
+        }
+    }
+
+}
+#endif
--- src/make.h
+++ src/make.h
@@ -20,6 +20,7 @@ int target(Make m, const char * name, const char * dependencies) {
 
 
 
+
 int main(int argc, char ** argv) {
     Make m = make_init(argc, argv);
 
@@ -35,4 +36,42 @@ int main(int argc, char ** argv) {
         }
     }
 
+}
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fatal_error(char * error_message) {
+    fprintf(stderr, error_message);
+    exit(1);
+}
+
+/*static int os_argc;
+static char ** os_argv = NULL;*/
+static char ** make_target = NULL;
+static char make_incomplete = 1;
+
+void init_make(int argc, char** argv) {
+    if(argc != 2) {
+        fatal_error("Wrong amount of cli arguments provided");
+    } else {
+        make_target = argv[1];
+    }
+}
+
+void target_internal(char * name, char * dependencies) {
+    
+}
+
+#define make \
+    while(make_incomplete)
+
+int main(int argc, char** argv) {
+    make {
+        target("test", "main") {
+            
+        }
+    }
 }
